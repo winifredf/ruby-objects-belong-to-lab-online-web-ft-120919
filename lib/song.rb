9511f6d8a866eefc147eb6be9461161af7312612
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-09 01:40:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :title, :name
+end
+
+  def initialize(title, name)
+    @title = title
+    @name = name
+  end
+
+love_on_top.artist
+  
+beonce = Artist.new("Beyonce", "R&B")
+love_on_top = Song.new("Love on Top")
+
+  
+
+
+
